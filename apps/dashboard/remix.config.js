693d9ec919a8d68d7353e0484acdb420c7abf976
Dimitrios Lytras <dnlytras@gmail.com>
2023-10-14 17:46:10 +0300

    chore(dashboard): remove unused bundled packages

--- apps/dashboard/remix.config.js
+++ apps/dashboard/remix.config.js
@@ -4,13 +4,7 @@ const {flatRoutes} = require('remix-flat-routes');
 module.exports = {
   serverModuleFormat: 'cjs',
   serverDependenciesToBundle: [
-    'axios',
     '@white-label/ui-core',
-    // todo: replace bcrypt with something else
-    // https://github.com/kelektiv/node.bcrypt.js/issues/964
-    'nock',
-    'mock-aws-s3',
-    'aws-sdk',
     '@sindresorhus/slugify',
     'escape-string-regexp',
     '@sindresorhus/transliterate',
