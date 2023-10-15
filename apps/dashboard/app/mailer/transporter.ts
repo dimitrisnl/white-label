9f4dea0f16f447fd78bdd9e4d2c929c02f04fb67
Dimitrios Lytras <dnlytras@gmail.com>
2023-10-15 11:59:12 +0300

    fix: re-use transporter

--- apps/dashboard/app/mailer/transporter.ts
+++ apps/dashboard/app/mailer/transporter.ts
@@ -2,13 +2,33 @@ import {createTransport} from 'nodemailer';
 
 import {config} from './config';
 
-export const transporter = createTransport({
-  host: config.SMTP_HOST,
-  port: config.SMTP_PORT,
-  secure: config.SMTP_SECURE === 'true',
-  auth: {
-    user: config.SMTP_USER,
-    pass: config.SMTP_PASSWORD,
-  },
-  pool: true,
-});
+function makeTransporter() {
+  return createTransport({
+    host: config.SMTP_HOST,
+    port: config.SMTP_PORT,
+    secure: config.SMTP_SECURE === 'true',
+    auth: {
+      user: config.SMTP_USER,
+      pass: config.SMTP_PASSWORD,
+    },
+    pool: true,
+  });
+}
+
+let transporter: ReturnType<typeof makeTransporter>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __transporter: ReturnType<typeof makeTransporter> | undefined;
+}
+
+if (process.env.NODE_ENV === 'production') {
+  transporter = makeTransporter();
+} else {
+  if (!global.__transporter) {
+    global.__transporter = makeTransporter();
+  }
+  transporter = global.__transporter;
+}
+
+export {transporter};
