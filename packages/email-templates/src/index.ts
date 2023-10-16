793079c96e29f60516dc3bee8b4d65b9e44489fa
Dimitrios Lytras <dnlytras@gmail.com>
2023-10-16 14:10:59 +0300

    refactor(email-templates): compile with ts

---
+++ packages/email-templates/src/index.ts
@@ -0,0 +1,5 @@
+export {render} from '@jsx-email/render';
+
+export {InvitationEmailTemplate} from './templates/invitation-email';
+export {PasswordResetEmailTemplate} from './templates/password-reset-email';
+export {VerificationEmailTemplate} from './templates/verification-email';
