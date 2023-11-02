0d4ecdc7c4322c6a4eec7726c0f91be0eded9e07
Dimitrios Lytras <dnlytras@gmail.com>
2023-11-02 19:56:45 +0200

    fix(app): use single dotenv import

--- apps/dashboard/app/entry.server.tsx
+++ apps/dashboard/app/entry.server.tsx
@@ -1,9 +1,10 @@
+import 'dotenv/config';
+
 /**
  * By default, Remix will handle generating the HTTP Response for you.
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/docs/en/main/file-conventions/entry.server
  */
-
 import type {EntryContext} from '@remix-run/node';
 import {createReadableStreamFromReadable} from '@remix-run/node';
 import {RemixServer} from '@remix-run/react';
